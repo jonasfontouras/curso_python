bffb5fca037ded3313c986d57b58c2fc3d4f14b8
jonasfontouras <jonasfontouras@gmail.com>
2020-10-12 19:44:16 -0300

    Operadores matemáticos com python

---
+++ operadores_matematicos.py
@@ -0,0 +1,12 @@
+num1 = int(input('Número 1: '))
+num2 = int(input('Número 2: '))
+
+soma = num1 + num2
+sub = num1 - num2
+mult = num1 * num2
+div = num1 / num2
+
+print('{} + {} = {}'.format(num1, num2, soma))
+print('{} - {} = {}'.format(num1, num2, sub))
+print('{} * {} = {}'.format(num1, num2, mult))
+print('{} / {} = {}'.format(num1, num2, div))
